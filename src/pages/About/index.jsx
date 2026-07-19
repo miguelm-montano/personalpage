672588588fbab798df197a32169b082fbc5c8de0
Miguel Montaño <miiguel2m@gmail.com>
2026-07-19 13:58:24 +0200

    feat: adding pages of the menu, adding menu options and add animations

---
+++ src/pages/About/index.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <p className="font-unbounded text-sm tracking-widest opacity-30">About me</p>
+    </div>
+  )
+}
